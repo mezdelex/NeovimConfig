e5e9ec70101082fe69edfaf12a0ca2cc743568ce
Mezdelex <alexcondegomez@gmail.com>
2022-12-18 17:02:49 +0100

    Added lspsaga, changed to main branches in every plugin, combined lspzero with lspsaga to achieve IDE UI experience and kept Telescope for grep and search tasks

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -16,18 +16,19 @@ return require('packer').startup(function(use)
     use('theprimeagen/harpoon')
     use('tpope/vim-fugitive')
     use('wbthomason/packer.nvim')
-    use({ 'VonHeikemen/lsp-zero.nvim', requires = {
-        { 'L3MON4D3/LuaSnip' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-nvim-lua' },
-        { 'hrsh7th/cmp-path' },
-        { 'hrsh7th/nvim-cmp' },
-        { 'neovim/nvim-lspconfig' },
-        { 'rafamadriz/friendly-snippets' },
-        { 'saadparwaiz1/cmp_luasnip' },
-        { 'williamboman/mason-lspconfig.nvim' },
-        { 'williamboman/mason.nvim' },
-    }
+    use('VonHeikemen/lsp-zero.nvim', {
+        requires = {
+            'L3MON4D3/LuaSnip',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            'hrsh7th/cmp-path',
+            'hrsh7th/nvim-cmp',
+            'neovim/nvim-lspconfig',
+            'rafamadriz/friendly-snippets',
+            'saadparwaiz1/cmp_luasnip',
+            'williamboman/mason-lspconfig.nvim',
+            'williamboman/mason.nvim',
+        }
     })
 end)
