fbda844090d33e9a20d55daaf9f0adf16b1bcf94
Mezdelex <alexcondegomez@gmail.com>
2022-12-18 20:34:03 +0100

    Deleted unused keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -35,13 +35,5 @@ vim.keymap.set('n', 'J', 'mzJ`z', opts)
 vim.keymap.set('v', 'J', ":m'>+1<CR>gv=gv", opts)
 vim.keymap.set('v', 'K', ":m'<-2<CR>gv=gv", opts)
 
-vim.keymap.set('n', '<leader>d', '"_d', opts)
-vim.keymap.set('v', '<leader>d', '"_d', opts)
-vim.keymap.set('x', '<leader>p', '"_dP', opts)
-
-vim.keymap.set('n', '<leader>Y', '"+Y', opts)
-vim.keymap.set('n', '<leader>y', '"+y', opts)
-vim.keymap.set('v', '<leader>y', '"+y', opts)
-
 vim.keymap.set('n', '<A-/>', ':%s/<C-r><C-w>/<C-r><C-w>', opts)
 vim.keymap.set('v', '<A-/>', ':s/^\\(.*\\)/\\1<left><left>', opts)
