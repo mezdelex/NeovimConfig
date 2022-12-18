2fcc699742802efbe9a71d4f21c344a7b9b03c66
Mezdelex <alexcondegomez@gmail.com>
2022-12-18 02:02:39 +0100

    Changed keymappings based on expected usage

--- lua/after/plugins/fugitive.lua
+++ lua/after/plugins/fugitive.lua
@@ -1,1 +1,1 @@
-vim.keymap.set('n', '<leader>g', vim.cmd.Git)
+vim.keymap.set('n', '<leader>G', vim.cmd.Git)
--- lua/after/plugins/telescope.lua
+++ lua/after/plugins/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>F', builtin.live_grep)
-vim.keymap.set('n', '<leader>G', builtin.grep_string)
 vim.keymap.set('n', '<leader>f', builtin.find_files)
+vim.keymap.set('n', '<leader>g', builtin.grep_string)
