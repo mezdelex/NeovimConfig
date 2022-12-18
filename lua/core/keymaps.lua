d847d3c1c8878879e2a2ad55b81a7be85a6d965f
Mezdelex <alexcondegomez@gmail.com>
2022-12-18 03:53:08 +0100

    Changed the command inputs back to <cmd> to avoid execution messages from the C-[ Esc equivalent. Using stagged changes and commit done with Fugitive.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,21 +1,16 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader>n', vim.cmd.Ex)
+vim.keymap.set('n', '<A-C>', ':q<CR>')
+vim.keymap.set('n', '<A-Q>', ':qa!<CR>')
+vim.keymap.set('n', '<A-q>', ':qa<CR>')
 
-vim.keymap.set('n', '<A-C>', ':wqa<CR>')
 vim.keymap.set('n', '<C-S>', ':wa<CR>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 
-vim.keymap.set('n', '<A-q>', ':bd<CR>')
+vim.keymap.set('n', '<A-c>', ':bd<CR>')
 vim.keymap.set('n', '<S-Tab>', ':bp<CR>')
 vim.keymap.set('n', '<Tab>', ':bn<CR>')
 
-vim.keymap.set('n', '<A-h>', '<C-w>h')
-vim.keymap.set('n', '<A-j>', '<C-w>j')
-vim.keymap.set('n', '<A-k>', '<C-w>k')
-vim.keymap.set('n', '<A-l>', '<C-w>l')
-
-vim.keymap.set('n', '<A-c>', '<C-w>c')
 vim.keymap.set('n', '<A-i>', '<C-w>s')
 vim.keymap.set('n', '<A-o>', '<C-w>v')
 
@@ -23,6 +18,11 @@ vim.keymap.set('n', '<A-P>', '<C-w>10+')
 vim.keymap.set('n', '<A-U>', '<C-w>10-')
 vim.keymap.set('n', '<A-p>', '<C-w>10>')
 vim.keymap.set('n', '<A-u>', '<C-w>10<')
+
+vim.keymap.set('n', '<A-h>', '<C-w>h')
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
 
 vim.keymap.set('i', '<C-h>', '<left>')
 vim.keymap.set('i', '<C-j>', '<down>')
@@ -41,10 +41,10 @@ vim.keymap.set('n', '<leader>Y', '"+Y')
 vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>y', '"+y')
 
-vim.keymap.set('n', '<C-[>', vim.cmd.cnext)
-vim.keymap.set('n', '<C-]>', vim.cmd.cprev)
-vim.keymap.set('n', '<leader>[', vim.cmd.lnext)
-vim.keymap.set('n', '<leader>]', vim.cmd.lprev)
+vim.keymap.set('n', '<C-[>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<C-]>', '<cmd>cprev<CR>')
+vim.keymap.set('n', '<leader>[', '<cmd>lnext<CR>')
+vim.keymap.set('n', '<leader>]', '<cmd>lprev<CR>')
 
 vim.keymap.set('n', '<A-/>', ':%s/<C-r><C-w>/<C-r><C-w>')
 vim.keymap.set('v', '<A-/>', ':s/^\\(.*\\)/\\1<left><left>')
