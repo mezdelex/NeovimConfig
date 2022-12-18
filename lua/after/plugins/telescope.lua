10ad789c0c6f49e7d35056258ee4df2d42834583
Mezdelex <alexcondegomez@gmail.com>
2022-12-18 01:54:04 +0100

    Refactor of existing commands, changed cmd to Vim API format, added live_grep and modified string_grep, removed git_files, and mocked git conflict with Fugitive

--- lua/after/plugins/telescope.lua
+++ lua/after/plugins/telescope.lua
@@ -1,7 +1,5 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>F', function()
-    builtin.grep_string({ search = vim.fn.input('Grep > ') });
-end)
-vim.keymap.set('n', '<leader>G', builtin.git_files, {})
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>F', builtin.live_grep)
+vim.keymap.set('n', '<leader>G', builtin.grep_string)
+vim.keymap.set('n', '<leader>f', builtin.find_files)
