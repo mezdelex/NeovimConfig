c12ff330fdbb585544f8f0b564e9581fe7cd73e5
Mezdelex <alexcondegomez@gmail.com>
2022-12-21 07:34:39 +0100

    Fixed system clipboard commands

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -36,11 +36,11 @@ vim.keymap.set('i', '<C-j>', '<down>', opts)
 vim.keymap.set('i', '<C-k>', '<up>', opts)
 vim.keymap.set('i', '<C-l>', '<right>', opts)
 
+vim.keymap.set('v', '<leader>p', '"+p', opts)
+vim.keymap.set('v', '<leader>y', '"+y', opts)
+
 vim.keymap.set('n', 'J', 'mzJ`z', opts)
 vim.keymap.set('v', 'J', ":m'>+1<CR>gv=gv", opts)
 vim.keymap.set('v', 'K', ":m'<-2<CR>gv=gv", opts)
-
-vim.keymap.set('n', '<leader>p', '"+p', opts)
-vim.keymap.set('n', '<leader>y', '"+y', opts)
 
 vim.keymap.set('n', '<A-/>', ':%s/<C-r><C-w>/<C-r><C-w>', opts)
