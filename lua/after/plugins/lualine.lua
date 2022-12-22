09cec273534ceda0bb3217a543edc14e2222c9e9
Mezdelex <alexcondegomez@gmail.com>
2022-12-22 21:38:15 +0100

    Added gruvbox colorscheme options to lualine and some customizations

--- lua/after/plugins/lualine.lua
+++ lua/after/plugins/lualine.lua
@@ -1,32 +1,65 @@
+local colors = require('gruvbox.palette')
+
 require('lualine').setup {
     extensions = {},
     inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
+        lualine_a = { 'mode' },
+        lualine_b = { 'diagnostics' },
+        lualine_c = {},
+        lualine_x = {},
         lualine_y = {},
-        lualine_z = {}
+        lualine_z = { 'filename' }
     },
     inactive_winbar = {},
     options = {
         always_divide_middle = true,
         component_separators = { left = '', right = '' },
-        disabled_filetypes = { statusline = {}, winbar = {} },
+        disabled_filetypes = { 'packer', 'NvimTree', statusline = {}, winbar = {} },
         globalstatus = false,
         icons_enabled = true,
         ignore_focus = {},
         refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
         section_separators = { left = '', right = '' },
-        theme = 'gruvbox'
+        theme = {
+            command = {
+                a = { bg = colors.neutral_yellow, fg = colors.dark0, gui = 'bold' },
+                b = { bg = colors.dark0, fg = colors.neutral_yellow },
+                c = { bg = colors.dark0, fg = colors.neutral_yellow }
+            },
+            inactive = {
+                a = { bg = colors.gray, fg = colors.dark0, gui = 'bold' },
+                b = { bg = colors.dark0, fg = colors.gray },
+                c = { bg = colors.dark0, fg = colors.gray }
+            },
+            insert = {
+                a = { bg = colors.neutral_yellow, fg = colors.dark0, gui = 'bold' },
+                b = { bg = colors.dark0, fg = colors.neutral_yellow },
+                c = { bg = colors.dark0, fg = colors.neutral_yellow }
+            },
+            normal = {
+                a = { bg = colors.neutral_yellow, fg = colors.dark0, gui = 'bold' },
+                b = { bg = colors.dark0, fg = colors.neutral_yellow },
+                c = { bg = colors.dark0, fg = colors.neutral_yellow }
+            },
+            replace = {
+                a = { bg = colors.neutral_yellow, fg = colors.dark0, gui = 'bold' },
+                b = { bg = colors.dark0, fg = colors.neutral_yellow },
+                c = { bg = colors.dark0, fg = colors.neutral_yellow }
+            },
+            visual = {
+                a = { bg = colors.neutral_yellow, fg = colors.dark0, gui = 'bold' },
+                b = { bg = colors.dark0, fg = colors.neutral_yellow },
+                c = { bg = colors.dark0, fg = colors.neutral_yellow }
+            }
+        }
     },
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { 'encoding', 'fileformat' },
+        lualine_z = { 'filename' }
     },
     tabline = {},
     winbar = {}
