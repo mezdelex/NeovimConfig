20abdb16f76e4d3109e8e7fe4c0bf21c41327be7
Mezdelex <alexcondegomez@gmail.com>
2022-12-23 10:48:15 +0100

    Changed format keymap to <C-a> due to weird bug with <C-k>

--- lua/after/plugins/lspzero.lua
+++ lua/after/plugins/lspzero.lua
@@ -14,7 +14,7 @@ lsp.on_attach(function(_, bufnr)
         virtual_text = true
     })
 
-    vim.keymap.set('n', '<C-f>', function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set('n', '<C-a>', function() vim.lsp.buf.format() end, opts)
     vim.keymap.set('n', '<leader>r', function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
