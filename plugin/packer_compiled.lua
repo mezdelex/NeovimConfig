4c063ea9b61a929fa360181d4cf68d5cc8a757ad
Mezdelex <alexcondegomez@gmail.com>
2022-12-27 22:57:02 +0100

    Updated plugins and removed Fugitive compiled binaries

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -213,11 +213,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "C:\\Users\\Mezdelex\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\undotree",
     url = "https://github.com/mbbill/undotree"
-  },
-  ["vim-fugitive"] = {
-    loaded = true,
-    path = "C:\\Users\\Mezdelex\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-fugitive",
-    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
