d37ba30c066a8b7d61d0aad3cd124f2b0f576d4e
Mezdelex <alexcondegomez@gmail.com>
2023-01-13 21:27:56 +0100

    Removed lspoutline from lualine since I don't really use that option

--- lua/after/plugins/lualine.lua
+++ lua/after/plugins/lualine.lua
@@ -14,7 +14,7 @@ require('lualine').setup {
     options = {
         always_divide_middle = true,
         component_separators = { left = '', right = '' },
-        disabled_filetypes = { 'lspsagaoutline', 'NvimTree', 'packer', statusline = {}, winbar = {} },
+        disabled_filetypes = { 'NvimTree', 'packer', statusline = {}, winbar = {} },
         globalstatus = false,
         icons_enabled = true,
         ignore_focus = {},
