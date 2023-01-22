ea3a3205d4689ad6b262d3b191ed45ce5c4cfbc9
Mezdelex <alexcondegomez@gmail.com>
2023-01-22 11:26:09 +0100

    removed require_installed TS highlighting support languages to allow automatic installation when required for the first time

--- lua/after/plugins/treesitter.lua
+++ lua/after/plugins/treesitter.lua
@@ -1,29 +1,5 @@
 require('nvim-treesitter.configs').setup {
     auto_install = true,
-    ensure_installed = {
-        'c_sharp',
-        'cpp',
-        'css',
-        'dockerfile',
-        'html',
-        'javascript',
-        'json',
-        'json5',
-        'jsonc',
-        'lua',
-        'markdown',
-        'php',
-        'python',
-        'regex',
-        'rust',
-        'scss',
-        'sql',
-        'toml',
-        'tsx',
-        'typescript',
-        'vue',
-        'yaml'
-    },
     highlight = {
         additional_vim_regex_highlighting = false,
         enable = true
