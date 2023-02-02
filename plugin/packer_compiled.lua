871fcf66cc68dc3937b01d30ab3473b3dbef25d0
Mezdelex <alexcondegomez@gmail.com>
2023-02-02 23:30:06 +0100

    removed indent-blankline, kekw

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -124,11 +124,6 @@ _G.packer_plugins = {
     path = "C:\\Users\\Mezdelex\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\gruvbox.nvim",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
-  ["indent-blankline.nvim"] = {
-    loaded = true,
-    path = "C:\\Users\\Mezdelex\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\indent-blankline.nvim",
-    url = "https://github.com/lukas-reineke/indent-blankline.nvim"
-  },
   ["lsp-zero.nvim"] = {
     loaded = true,
     path = "C:\\Users\\Mezdelex\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\lsp-zero.nvim",
