c62eedf6c0652a4a04092aad5013207a1fb609d6
Mezdelex <alexcondegomez@gmail.com>
2023-03-25 10:07:14 +0100

    updates and formatting fix

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
     'lewis6991/gitsigns.nvim',
     config = function()
         require('gitsigns').setup({
-            current_line_blame           = true,
+            current_line_blame = true,
             current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> <summary>'
         })
     end
