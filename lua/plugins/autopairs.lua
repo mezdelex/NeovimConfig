43ef93411771255ea350a6cb8a3182f6afa45da5
Mezdelex <alexcondegomez@gmail.com>
2023-08-10 08:19:02 +0200

    switched to autotags and autopairs default config (which was what I was using anyways)

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,19 +1,6 @@
 return {
     'windwp/nvim-autopairs',
     config = function()
-        require('nvim-autopairs').setup({
-            break_undo = true,
-            check_ts = false,
-            disable_filetype = { 'TelescopePrompt' },
-            disable_in_macro = false,
-            disable_in_replace_mode = true,
-            disable_in_visualblock = false,
-            enable_abbr = false,
-            enable_afterquote = true,
-            enable_bracket_in_quote = true,
-            enable_check_bracket_line = true,
-            enable_moveright = true,
-            ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=]
-        })
+        require('nvim-autopairs').setup()
     end
 }
--- lua/plugins/autotags.lua
+++ lua/plugins/autotags.lua
@@ -1,11 +1,6 @@
 return {
     'windwp/nvim-ts-autotag',
     config = function()
-        require('nvim-ts-autotag').setup({
-            filetypes = { 'glimmer', 'handlebars', 'hbs', 'html', 'javascript', 'javascriptreact', 'jsx', 'markdown',
-                'php', 'rescript', 'svelte', 'tsx', 'typescript', 'typescriptreact', 'vue', 'xml' },
-            skip_tags = { 'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link',
-                'menuitem', 'meta', 'param', 'slot', 'source', 'track', 'wbr' }
-        })
+        require('nvim-ts-autotag').setup()
     end
 }
