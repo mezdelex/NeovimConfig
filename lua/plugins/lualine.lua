75360b0611e30c3c6ab7df98458a187c3addfd95
Mezdelex <alexcondegomez@gmail.com>
2023-08-19 21:11:47 +0200

    improved lualine appearance and code refactor

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,34 +1,40 @@
 return {
     'nvim-lualine/lualine.nvim',
     config = function()
+        local bold = { gui = 'bold' }
         local colors = require('gruvbox.palette').get_base_colors(vim.o.background)
-        local diff_source = function()
-            local gitsigns = vim.b.gitsigns_status_dict
-
-            if gitsigns then
-                return { added = gitsigns.added, modified = gitsigns.changed, removed = gitsigns.removed }
-            end
-        end
+        local sections = {
+            diagnostics = { 'diagnostics', update_in_insert = true },
+            diff = {
+                'diff',
+                diff_color = {
+                    added = { fg = colors.green },
+                    modified = { fg = colors.yellow },
+                    removed = { fg = colors.red }
+                },
+                symbols = { added = ' ', modified = ' ', removed = ' ' }
+            }
+        }
         local templates = {
             default = {
-                a = { bg = colors.neutral_yellow, fg = colors.bg0, gui = 'bold' },
-                b = { bg = colors.bg1, fg = colors.yellow },
-                c = { bg = colors.bg1, fg = colors.yellow }
+                a = { bg = colors.neutral_yellow, fg = colors.bg0, bold },
+                b = { bg = colors.bg2, fg = colors.yellow, bold },
+                c = { bg = colors.bg1, fg = colors.yellow, bold }
             },
             inactive = {
-                a = { bg = colors.gray, fg = colors.bg0, gui = 'bold' },
-                b = { bg = colors.bg1, fg = colors.gray },
-                c = { bg = colors.bg1, fg = colors.gray }
+                a = { bg = colors.gray, fg = colors.bg0, bold },
+                b = {},
+                c = { bg = colors.bg1, fg = colors.yellow, bold }
             }
         }
 
         require('lualine').setup({
             inactive_sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { { 'diff', source = diff_source } },
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = { { 'diagnostics', update_in_insert = true } },
+                lualine_b = {},
+                lualine_c = { sections.diff },
+                lualine_x = { sections.diagnostics },
+                lualine_y = {},
                 lualine_z = { 'filename' }
             },
             options = {
@@ -54,10 +60,10 @@ return {
             },
             sections = {
                 lualine_a = { 'mode' },
-                lualine_b = { 'branch', { 'diff', source = diff_source } },
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = { { 'diagnostics', update_in_insert = true }, 'progress' },
+                lualine_b = { 'branch' },
+                lualine_c = { sections.diff },
+                lualine_x = { sections.diagnostics },
+                lualine_y = { 'progress' },
                 lualine_z = { 'filename' }
             }
         })
