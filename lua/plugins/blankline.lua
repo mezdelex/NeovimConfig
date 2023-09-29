59701c4446c79ab546477d49a52a370dc6144346
Mezdelex <alexcondegomez@gmail.com>
2023-09-29 09:15:26 +0200

    turned off the default scope indicator in the new changes

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,6 +1,8 @@
 return {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-        require('ibl').setup()
+        require('ibl').setup({
+            scope = { enabled = false }
+        })
     end
 }
