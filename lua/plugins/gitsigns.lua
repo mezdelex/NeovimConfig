50e14e05a98eac4fc250a72c0b9aa9ad68e7b413
Mezdelex <alexcondegomez@gmail.com>
2023-10-31 09:15:17 +0100

    small change to gitsigns line blame formatting

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         require('gitsigns').setup({
             current_line_blame = true,
-            current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> <summary>'
+            current_line_blame_formatter = '<author> <author_time:%d-%m-%Y> <summary>'
         })
     end
 }
