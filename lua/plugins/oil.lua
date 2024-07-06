f12d6140979c73bd3316c76f99f2f8510a2457b1
Mezdelex <alexcondegomez@gmail.com>
2024-07-06 04:29:02 +0200

    feat(lsp-file-operations): added lsp-file-operations to oil.lua to handle affected dependencies on file renaming and tree changes

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,7 +7,9 @@ return {
             cleanup_delay_ms = 250,
             view_options = { show_hidden = true }
         })
+        require('lsp-file-operations').setup()
 
         vim.keymap.set('n', '<leader>o', ':Oil<cr>', opts)
-    end
+    end,
+    dependencies = { 'antosha417/nvim-lsp-file-operations' }
 }
