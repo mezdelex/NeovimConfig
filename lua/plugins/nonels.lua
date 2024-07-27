658b9dc9f257136fe7c48c1dc1c05ec4a9e95211
Mezdelex <alexcondegomez@gmail.com>
2024-07-27 11:25:17 +0200

    feat(nonels): added sql_formatter and shfmt to nonels

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,14 +1,16 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.csharpier,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.stylua,
-			},
-		})
-	end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.csharpier,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.shfmt,
+                null_ls.builtins.formatting.sql_formatter,
+                null_ls.builtins.formatting.stylua,
+            },
+        })
+    end,
 }
