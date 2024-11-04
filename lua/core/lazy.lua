27bd2c6c2cd0e47e7e80b113dbec1d35d8acfa0b
Mezdelex <alexcondegomez@gmail.com>
2024-11-04 19:52:50 +0100

    feat(lazy): removed annoying change detection notification

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -12,4 +12,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+	change_detection = { notify = false },
+})
