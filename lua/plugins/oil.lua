024a88adebae3f30ab61056c2450756914b5bee7
Alejandro Conde Gómez <alexcondegomez@gmail.com>
2025-03-11 22:29:49 +0100

    refactor(cleanup): removed unneeded dependencies

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,12 +2,9 @@ return {
     "stevearc/oil.nvim",
     config = function()
         require("oil").setup({
-            cleanup_delay_ms = 250,
             view_options = { show_hidden = true },
         })
-        require("lsp-file-operations").setup()
 
         vim.keymap.set("n", "<leader>o", "<cmd>Oil<cr>")
     end,
-    dependencies = { "antosha417/nvim-lsp-file-operations" },
 }
