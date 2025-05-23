bde6b916e1cd760e242310986f78731ed310035a
Alejandro Conde Gómez <alexcondegomez@gmail.com>
2025-05-24 00:49:56 +0200

    chore(codecompanion): switched from copilot to gemini adapter; removed copilot.vim

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,15 +1,12 @@
 return {
     "olimorris/codecompanion.nvim",
     config = function()
-        vim.g.copilot_enabled = false
-
         require("codecompanion").setup({
             strategies = {
-                adapters = { chat = "copilot" },
+                chat = { adapter = "gemini" },
             },
         })
 
         vim.keymap.set("n", "<leader>h", "<cmd>CodeCompanionChat<cr>")
     end,
-    dependencies = { "github/copilot.vim" },
 }
