3edc8015d862660b26177f7b011e43bd750cfeb2
Alejandro Conde Gómez <alexcondegomez@gmail.com>
2025-07-29 20:28:50 +0200

    feat(textobjects): added treesitter textobjects for convenient extra lsp based movement/selections plus builtin incremental selection

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,8 +1,8 @@
-vim.api.nvim_create_augroup("init", { clear = true })
+vim.api.nvim_create_augroup("core", { clear = true })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(event)
-        local options = { buffer = event.buf }
+    callback = function(args)
+        local options = { buffer = args.buf }
 
         vim.keymap.set("n", "<a-f>", vim.lsp.buf.format, options)
         vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, options)
@@ -12,17 +12,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "gr", vim.lsp.buf.references, options)
         vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, options)
     end,
-    group = "init",
+    group = "core",
 })
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank({ timeout = 100 })
     end,
-    group = "init",
+    group = "core",
 })
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         vim.cmd("clearjumps")
     end,
-    group = "init",
+    group = "core",
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,72 @@ return {
     config = function()
         local fallback = vim.filetype.get_option
         local options = { remap = true }
+        local textobjects = {
+            ci = "@class.inner",
+            co = "@class.outer",
+            fi = "@function.inner",
+            fo = "@function.outer",
+            ii = "@conditional.inner",
+            io = "@conditional.outer",
+            li = "@loop.inner",
+            lo = "@loop.outer",
+        }
 
         require("nvim-treesitter.configs").setup({
             auto_install = true,
             highlight = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<cr>",
+                    node_decremental = "<bs>",
+                    node_incremental = "<cr>",
+                },
+            },
+            textobjects = {
+                move = {
+                    enable = true,
+                    goto_next_end = {
+                        ["]C"] = textobjects.co,
+                        ["]F"] = textobjects.fo,
+                        ["]I"] = textobjects.io,
+                        ["]L"] = textobjects.lo,
+                    },
+                    goto_next_start = {
+                        ["]c"] = textobjects.co,
+                        ["]f"] = textobjects.fo,
+                        ["]i"] = textobjects.io,
+                        ["]l"] = textobjects.lo,
+                    },
+                    goto_previous_end = {
+                        ["[C"] = textobjects.co,
+                        ["[F"] = textobjects.fo,
+                        ["[I"] = textobjects.io,
+                        ["[L"] = textobjects.lo,
+                    },
+                    goto_previous_start = {
+                        ["[c"] = textobjects.co,
+                        ["[f"] = textobjects.fo,
+                        ["[i"] = textobjects.io,
+                        ["[l"] = textobjects.lo,
+                    },
+                    set_jumps = true,
+                },
+                select = {
+                    enable = true,
+                    keymaps = {
+                        ["ac"] = textobjects.co,
+                        ["af"] = textobjects.fo,
+                        ["ai"] = textobjects.io,
+                        ["al"] = textobjects.lo,
+                        ["ic"] = textobjects.ci,
+                        ["if"] = textobjects.fi,
+                        ["ii"] = textobjects.ii,
+                        ["il"] = textobjects.li,
+                    },
+                    lookahead = true,
+                },
+            },
         })
         require("ts_context_commentstring").setup({
             enable_autocmd = false,
@@ -20,5 +82,5 @@ return {
         vim.keymap.set("n", "<a-/>", "gcc", options)
         vim.keymap.set("v", "<a-/>", "gc", options)
     end,
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", "nvim-treesitter/nvim-treesitter-textobjects" },
 }
