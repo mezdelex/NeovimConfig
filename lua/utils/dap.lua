e8f1c887b53fc3d87e932016b3b2a4c4ccab39ed
mezdelex <alexcondegomez@gmail.com>
2025-08-28 23:21:57 +0200

    refactor(utils.dap): refactor of utils dap

--- lua/utils/dap.lua
+++ lua/utils/dap.lua
@@ -1,3 +1,33 @@
+---@param dir string
+---@param latest_file string?
+---@param latest_time number
+---@return string?, number
+local function scan_dir(dir, latest_file, latest_time)
+	local fd = vim.uv.fs_scandir(dir)
+	if not fd then
+		return latest_file, latest_time
+	end
+
+	while true do
+		local name, t = vim.uv.fs_scandir_next(fd)
+		if not name then
+			break
+		end
+
+		local path = dir .. "/" .. name
+		if t == "directory" then
+			latest_file, latest_time = scan_dir(path, latest_file, latest_time)
+		elseif (name:match("%.dll$") or name:match("%.exe$")) and not name:match("^System") then
+			local st = vim.uv.fs_stat(path)
+			if st and st.mtime.sec > latest_time then
+				latest_file, latest_time = path, st.mtime.sec
+			end
+		end
+	end
+
+	return latest_file, latest_time
+end
+
 ---@class Utils.Dap
 M = {
 	---@param base_paths string[]
@@ -10,32 +40,8 @@ M = {
 		end, base_paths)
 		local latest_file, latest_time = nil, 0
 
-		local function scan_dir(dir)
-			local fd = vim.uv.fs_scandir(dir)
-			if not fd then
-				return
-			end
-
-			while true do
-				local name, t = vim.uv.fs_scandir_next(fd)
-				if not name then
-					break
-				end
-
-				local path = dir .. "/" .. name
-				if t == "directory" then
-					scan_dir(path)
-				elseif (name:match("%.dll$") or name:match("%.exe$")) and not name:match("^System") then
-					local st = vim.uv.fs_stat(path)
-					if st and st.mtime.sec > latest_time then
-						latest_file, latest_time = path, st.mtime.sec
-					end
-				end
-			end
-		end
-
 		for _, dir in ipairs(build_dirs) do
-			scan_dir(dir)
+			latest_file, latest_time = scan_dir(dir, latest_file, latest_time)
 		end
 
 		return latest_file and latest_file:match("(.*/)[^/]+$") or cwd
