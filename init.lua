45a073e52727ffd03d3ffead5152218718a37fe1
mezdelex <alexcondegomez@gmail.com>
2025-08-29 14:24:57 +0200

    feat(cache): added vim.loader.enable() to cache compiled specs and improve loading times

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ local utils_pack = require("utils.pack")
 vim.api.nvim_create_user_command("PackBuild", utils_pack.build, {})
 vim.api.nvim_create_user_command("PackUpdate", utils_pack.update, {})
 vim.g.mapleader = " "
+vim.loader.enable()
 
 utils_pack.load()
