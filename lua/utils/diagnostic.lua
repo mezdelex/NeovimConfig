5b54b562f9b91006b6fab094a24f1ec0a24149be
mezdelex <alexcondegomez@gmail.com>
2025-09-19 13:18:36 +0200

    perf(winid): move winid/open_float check to on_jump callback to avoid performing that check when there's no diagnostic to jump to

--- lua/utils/diagnostic.lua
+++ lua/utils/diagnostic.lua
@@ -5,13 +5,13 @@ local M = {} ---@class Utils.Diagnostic
 
 ---@param count number
 M.jump = function(count)
-    if winid and vim.api.nvim_win_is_valid(winid) then
-        vim.api.nvim_win_close(winid, true)
-    end
-
     vim.diagnostic.jump({
         count = count,
         on_jump = function()
+            if winid and vim.api.nvim_win_is_valid(winid) then
+                vim.api.nvim_win_close(winid, true)
+            end
+
             _, winid = vim.diagnostic.open_float({ scope = "cursor" })
         end,
     })
