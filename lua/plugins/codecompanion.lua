b5434ac70db02e5458ebb140a70e6c926133313f
mezdelex <alexcondegomez@gmail.com>
2025-12-05 18:15:30 +0100

    chore(codecompanion): set ignore_warnings = true temporarily to mute breaking changes message for the incoming v18 merge

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,21 +1,22 @@
 return {
-	config = function()
-		local codecompanion = require("codecompanion")
+    config = function()
+        local codecompanion = require("codecompanion")
 
-		codecompanion.setup({
-			strategies = {
-				chat = { adapter = "gemini" },
-			},
-		})
+        codecompanion.setup({
+            ignore_warnings = true,
+            strategies = {
+                chat = { adapter = "gemini" },
+            },
+        })
 
-		vim.keymap.set("n", "<leader>h", codecompanion.chat)
-	end,
-	defer = true,
-	dependencies = {
-		{
-			defer = true,
-			src = "https://github.com/nvim-lua/plenary.nvim",
-		},
-	},
-	src = "https://github.com/olimorris/codecompanion.nvim",
+        vim.keymap.set("n", "<leader>h", codecompanion.chat)
+    end,
+    defer = true,
+    dependencies = {
+        {
+            defer = true,
+            src = "https://github.com/nvim-lua/plenary.nvim",
+        },
+    },
+    src = "https://github.com/olimorris/codecompanion.nvim",
 }
