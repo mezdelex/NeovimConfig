c71ec740667532ce3b848f4091f6f686d19ae966
mezdelex <alexcondegomez@gmail.com>
2025-12-21 12:23:27 +0100

    chore(unpack): added vim.opt.runtimepath:prepend(...) call to the unpack install process to avoid module not found error message on first load; see https://github.com/mezdelex/unpack

--- lua/core/unpack.lua
+++ lua/core/unpack.lua
@@ -1,13 +1,14 @@
 local unpack_path = vim.fn.stdpath("data") .. "/site/pack/managers/start/unpack"
 
 if not vim.uv.fs_stat(unpack_path) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/mezdelex/unpack",
-		unpack_path,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/mezdelex/unpack",
+        unpack_path,
+    })
 end
+vim.opt.runtimepath:prepend(unpack_path)
 
 require("unpack").setup()
