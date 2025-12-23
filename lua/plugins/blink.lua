d963104f6860120548aedc27505bc73b47b1028e
mezdelex <alexcondegomez@gmail.com>
2025-12-23 22:05:08 +0100

    chore(blink,unpack): added conflicting files from blink.cmp due to WinOS lock to the new unpack hook 'conflicts' to be handled properly

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,13 +1,16 @@
 return {
-	config = function()
-		require("blink.cmp").setup({
-			completion = {
-				documentation = { auto_show = true },
-			},
-			keymap = { preset = "enter" },
-		})
-	end,
-	data = { build = "cargo build --release" },
-	defer = true,
-	src = "https://github.com/saghen/blink.cmp",
+    config = function()
+        require("blink.cmp").setup({
+            completion = {
+                documentation = { auto_show = true },
+            },
+            keymap = { preset = "enter" },
+        })
+    end,
+    data = {
+        build = "cargo build --release",
+        conflicts = { "blink_cmp_fuzzy.dll" },
+    },
+    defer = true,
+    src = "https://github.com/saghen/blink.cmp",
 }
