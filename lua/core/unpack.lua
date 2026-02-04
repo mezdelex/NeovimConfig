cace9ace44b8984e8c51539b68b80c2586cd0ea9
mezdelex <alexcondegomez@gmail.com>
2026-02-04 22:36:45 +0100

    chore(unpack): start using unpack via vim.pack directly

--- lua/core/unpack.lua
+++ lua/core/unpack.lua
@@ -1,11 +1,1 @@
-local unpack_path = vim.fn.stdpath("data") .. "/site/pack/managers/start/unpack"
-
-if not vim.uv.fs_stat(unpack_path) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/mezdelex/unpack.nvim",
-        unpack_path,
-    })
-end
+vim.pack.add({ "https://github.com/mezdelex/unpack.nvim" }, { confirm = false })
